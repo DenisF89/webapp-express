a65ca449cc5a4f021a6674a0eaea0aa2e05e5898
Denis89 <filippinidenis@gmail.com>
2026-03-23 17:18:55 +0100

    aggiunto destroy

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,10 +1,10 @@
 const db = require("../data/db");
 
 function index(req,res){
-    const sqlQuery = 'SELECT * FROM movies';
+    const sqlQuery = `SELECT * FROM movies`;
     db.query(sqlQuery)
-            .then(results=>{
-                return res.status(200).json(results[0]);
+            .then(([movies])=>{ //destructuring: results[values,fields] => [movies]=results[0]
+                return res.status(200).json(movies);
             })
             .catch(error=>{
                 return res.status(500).json({  error:"Database connection error",
@@ -15,14 +15,14 @@ function index(req,res){
 
 function show(req,res){
     const id = Number(req.params.id);
-    const sqlMovies = 'SELECT * FROM movies WHERE id = ?';
-    const sqlReviews = 'SELECT * FROM reviews WHERE movie_id = ?';
+    const sqlMovies = `SELECT * FROM movies WHERE id = ?`;
+    const sqlReviews = `SELECT * FROM reviews WHERE movie_id = ?`;
     db.query(sqlMovies,[id])
-            .then(movies=>{
-                const rows = movies[0];
-                if (rows.length === 0)
+            .then(([movies])=>{
+                //const rows = movies[0];
+                if (movies.length === 0)
                     {return res.status(404).json({error:"Not found",message:"Film non trovato"})}
-                const movie = rows[0];
+                const movie = movies[0];
                 return db.query(sqlReviews,[id])
                     .then(reviews=>{
                         movie.reviews = reviews[0];
@@ -38,4 +38,17 @@ function show(req,res){
 
 }
 
-module.exports = {index,show};
+function destroy(req,res){
+    const id = Number(req.params.id);
+    const sqlQuery =`DELETE from movies WHERE movies.id = ?`;
+    db.query(sqlQuery,[id])
+        .then(([result])=>{ 
+            if (result.affectedRows === 0)
+                {return res.status(404).json({error:"Not found",message:"Film da eliminare non trovato"});}
+            return res.sendStatus(204);
+        }).catch(error=>{
+            return res.status(500).json({error: "Database internal Error", message:"Nessun film eliminato"}); 
+        });
+}
+
+module.exports = {index,show,destroy};
